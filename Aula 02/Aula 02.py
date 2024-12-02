457d3976f2c3269969284e1688444562c62eb9b9
Xamueldolove <jvdiasdossantos53@gmail.com>
2024-12-02 10:01:18 -0300

    Add files via upload
    Posso ser sincero? Não aprendi muita coisa, mas tu, professor, é um cara excelente!

---
+++ Aula 02/Aula 02.py
@@ -0,0 +1,10 @@
+print("Não quero minha ex de volta")
+
+nome_usuario = "João Vitor Perigoso" #tipo -> str = string
+idade_usuario = 19 # tipo -> int = integer 
+altura_usuario = 1.86 # tipo -> float
+maior_de_idade = True # tipo -> bool = boolean
+
+print("Seja bem-vindo", nome_usuario, "sua idade é", idade_usuario, "sua altura é", altura_usuario, "Voce é maior de idade?", maior_de_idade)
+
+
